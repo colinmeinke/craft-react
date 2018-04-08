26b2bd737b7a05c32ed8c9a5238f017b81707098
Colin Meinke <hello@colinmeinke.com>
2018-04-08 20:28:31 -0300

    abort fetch on unmount and handle errors

--- ui/client.js
+++ ui/client.js
@@ -6,26 +6,49 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 class Page extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { loading: true, title: null, content: null }
+    this.state = { loading: true, failed: false, title: null, content: null }
   }
 
   componentDidMount() {
-    fetch(this.props.url)
-      .then(res => res.json())
+    this.controller = {}
+
+    if ("AbortController" in self) {
+      this.controller = new AbortController()
+    }
+
+    fetch(this.props.url, { signal: this.controller.signal })
+      .then(res => {
+        if (res.status !== 200) {
+          throw new Error()
+        }
+
+        return res.json()
+      })
       .then(({ title, content }) => {
         this.setState({ loading: false, title, content })
       })
+      .catch(() => this.setState({ loading: false, failed: true }))
+  }
+
+  componentWillUnmount() {
+    if (this.controller.abort === 'function') {
+      this.controller.abort()
+    }
   }
 
   render() {
     if (this.state.loading) {
       return <p>Loading...</p>
+    } else if (this.state.failed) {
+      return <p>Loading failed :(</p>
     }
 
-    return <main>
-      <h1>{ this.state.title }</h1>
-      <p>{ this.state.content }</p>
-    </main>
+    return (
+      <main>
+        <h1>{ this.state.title }</h1>
+        <p>{ this.state.content }</p>
+      </main>
+    )
   }
 }
 
